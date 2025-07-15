ebd71553903529d8cc361d65e66183192f65475c
Abdulmalik Alshahwan <173699456+Alshahwan570@users.noreply.github.com>
2025-07-15 21:45:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   console.log("log....");
   console.log("new ");
-  return <div>App Goo new my branch </div>;
+  return <div>App Goo new anything </div>;
 }
 
 export default App;
